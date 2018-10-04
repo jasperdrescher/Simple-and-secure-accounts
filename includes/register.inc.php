77ee2b3655cb264b0ab8ec7b472e66b29e20c0be
JasperDre <jasperdre@gmail.com>
2018-10-04 15:52:56 +0200

    Changed register page to fix redirect

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -66,7 +66,7 @@ if (isset($_POST['username'], $_POST['email'], $_POST['p']))
                 header('Location: ../error.php?err=Registration failure: INSERT');
             }
         }
-        header('Location: ./register_success.php');
+        header('Location: ./register_success.html');
         exit();
     }
 }
