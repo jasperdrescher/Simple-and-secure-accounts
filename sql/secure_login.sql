8fe168cef2519f1c80f458e48e194c92dbc37f6a
JasperDre <jasperdre@gmail.com>
2018-10-04 12:55:34 +0200

    Added create database

--- sql/secure_login.sql
+++ sql/secure_login.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 03, 2013 at 03:03 PM
--- Server version: 5.5.34
--- PHP Version: 5.3.10-1ubuntu3.8
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -14,6 +6,14 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+
+-- --------------------------------------------------------
+
+--
+-- Create database `secure_login`
+--
+
+CREATE DATABASE IF NOT EXISTS `secure_login`;
 
 --
 -- Database: `secure_login`
